554169fcb1b508aec37e918140c184d406ec91b4
Darshanproject <gadadarshan6184@gmail.com>
2023-11-22 12:18:56 +0530

    22-11-2023

---
+++ C language/Lecture 1/First.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+main(){
+	printf("\n \tHello world!! \n");
+	printf("Hello world!!");
+}
+
+// \t is used for tab spacing
+// \n is used for start from new line 
---
+++ C language/Lecture 1/second.c
@@ -0,0 +1,19 @@
+/*
+you have to use only apl in starting you cant use numbers 
+you can use numbers in ending 
+you cant use white space  you can use _ as you whitespace 
+you cant use any keyword as var
+*/
+#include<stdio.h>
+main(){
+	int num=5;  //%d
+	float numf =5.55;  //%f
+	double  numd=25.65; //%lf 
+	char name='A'; // %c
+	char name1[20]="Darshan"; //%s
+	printf("%d \n",num);
+	printf("%.2f\n",numf);
+	printf("%.2lf \n",numd);
+	printf("%c\n",name);
+	printf("%s\n",name1);
+}
